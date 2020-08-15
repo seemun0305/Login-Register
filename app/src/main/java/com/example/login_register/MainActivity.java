ba2dd8ffa6367b2d8449371a6822e5d923d94bbc
seemun <loongsm-wm19@student.tarc.edu.my>
2020-08-15 23:48:03 +0800

    commit by sm

--- app/src/main/java/com/example/login_register/MainActivity.java
+++ app/src/main/java/com/example/login_register/MainActivity.java
@@ -19,12 +19,16 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText inputEmail, inputPassword;
+    private EditText inputEmail, inputPassword,inputname;
     private Button btnSignIn, btnSignUp,btnresetPassword,btnBack;
     private FirebaseAuth auth;
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
 
 
     @Override
@@ -36,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
         btnSignUp = (Button) findViewById(R.id.btn_register);
         btnBack = (Button) findViewById(R.id.btnback);
         btnresetPassword = (Button) findViewById(R.id.btnreset);
+        inputname = (EditText) findViewById(R.id.et_name);
         inputEmail = (EditText) findViewById(R.id.et_email);
         inputPassword = (EditText) findViewById(R.id.et_password);
 
@@ -57,8 +62,13 @@ public class MainActivity extends AppCompatActivity {
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                rootNode = FirebaseDatabase.getInstance();
+                reference = rootNode.getReference("users");
+                String name = inputname.getText().toString().trim();
                 String email = inputEmail.getText().toString().trim();
                 String password = inputPassword.getText().toString().trim();
+                UserHelperClass helperClass = new UserHelperClass(name,email, password);
+                reference.child(name).setValue(helperClass);
                 if (TextUtils.isEmpty(email)) {
                     Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
                     return;
